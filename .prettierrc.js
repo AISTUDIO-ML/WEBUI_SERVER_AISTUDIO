bf502dc780b900a5267b3889f0d3bfcfa06f2241
simpleneeraj <teamsimpleneeraj@gmail.com>
2023-06-24 06:43:55 +0530

    Web UI for AIStudio

---
+++ .prettierrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  htmlWhitespaceSensitivity: 'css',
+  insertPragma: false,
+  bracketSameLine: false,
+  jsxSingleQuote: true,
+  printWidth: 120,
+  proseWrap: 'preserve',
+  quoteProps: 'as-needed',
+  requirePragma: false,
+  semi: false,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: 'none',
+  useTabs: false
+}
---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,117 @@
+// ** React Imports
+import { createContext, useEffect, useState, ReactNode } from "react"
+
+// ** Next Import
+import { useRouter } from "next/router"
+
+// ** Axios
+import axios from "axios"
+
+// ** Config
+import authConfig from "src/configs/auth"
+
+// ** Types
+import { AuthValuesType, LoginParams, ErrCallbackType, UserDataType } from "./types"
+import React from "react"
+
+// ** Defaults
+const defaultProvider: AuthValuesType = {
+  user: null,
+  loading: true,
+  setUser: () => null,
+  setLoading: () => Boolean,
+  login: () => Promise.resolve(),
+  logout: () => Promise.resolve()
+}
+
+const AuthContext = createContext(defaultProvider)
+
+type Props = {
+  children: ReactNode
+}
+
+const AuthProvider = ({ children }: Props) => {
+  // ** States
+  const [user, setUser] = useState<UserDataType | null>(defaultProvider.user)
+  const [loading, setLoading] = useState<boolean>(defaultProvider.loading)
+
+  // ** Hooks
+  const router = useRouter()
+
+  const initAuth = React.useCallback(async (): Promise<void> => {
+    const storedToken = window.localStorage.getItem(authConfig.storageTokenKeyName)!
+    if (storedToken) {
+      setLoading(true)
+      await axios
+        .get(authConfig.meEndpoint, {
+          headers: {
+            Authorization: storedToken
+          }
+        })
+        .then(async response => {
+          setLoading(false)
+          console.log(response.data)
+          setUser({ ...response.data.userData })
+        })
+        .catch(() => {
+          localStorage.removeItem("userData")
+          localStorage.removeItem("refreshToken")
+          localStorage.removeItem("accessToken")
+          setUser(null)
+          setLoading(false)
+          if (authConfig.onTokenExpiration === "logout" && !router.pathname.includes("login")) {
+            router.replace("/login")
+          }
+        })
+    } else {
+      setLoading(false)
+    }
+  }, [router])
+  useEffect(() => {
+    initAuth()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  const handleLogin = (params: LoginParams, errorCallback?: ErrCallbackType) => {
+    axios
+      .post(authConfig.loginEndpoint, params)
+      .then(async response => {
+        params.rememberMe
+          ? window.localStorage.setItem(authConfig.storageTokenKeyName, response.data.accessToken)
+          : null
+        const returnUrl = router.query.returnUrl
+
+        console.log(response.data)
+        setUser({ ...response.data.userData })
+        params.rememberMe ? window.localStorage.setItem("userData", JSON.stringify(response.data.userData)) : null
+
+        const redirectURL = returnUrl && returnUrl !== "/" ? returnUrl : "/"
+
+        router.replace(redirectURL as string)
+      })
+
+      .catch(err => {
+        if (errorCallback) errorCallback(err)
+      })
+  }
+
+  const handleLogout = () => {
+    setUser(null)
+    window.localStorage.removeItem("userData")
+    window.localStorage.removeItem(authConfig.storageTokenKeyName)
+    router.push("/login")
+  }
+
+  const values = {
+    user,
+    loading,
+    setUser,
+    setLoading,
+    login: handleLogin,
+    logout: handleLogout
+  }
+
+  return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>
+}
+
+export { AuthContext, AuthProvider }
